0346ff412627f790b2c17910e7ebcc7ae4241c4b
komali2 <rogersjcaleb@gmail.com>
2016-09-09 14:49:59 -0700

    feat: filling in reddit array on initial page load

--- client/js/index.js
+++ client/js/index.js
@@ -1,12 +1,25 @@
 'use strict'; // Allows block-scope and other useful es6 syntax
 
 // Main App Declaration
-// ngAnimate only dependency - needed to have Angular attach animation classes
+// ngAnimate dependency needed to have Angular attach animation classes
 var app = angular.module('redditApp', ['ngAnimate'])
   // Parent controller, highest scope in this app
   .controller('mainController', [
-    '$scope',
-    ($scope) => {
+    '$scope', '$http',
+    ($scope, $http) => {
+
+      $scope.reddit = [];
+      $http({
+        method: 'GET',
+        url: '/reddit'
+      }).then(function(response){
+        Array.prototype.push.apply($scope.reddit, response.data);
+      }, function(response){
+        console.log('Nope!', response);
+      });
+
+      $scope.$watch('reddit', (newVal)=>{
+      }, true);
 
 
     }]);
