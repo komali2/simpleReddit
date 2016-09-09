8b1e8b9646f1c8d742a84c0f5ba39b757b8dd235
komali2 <rogersjcaleb@gmail.com>
2016-09-09 14:00:03 -0700

    feat: sending subreddit list on get to reddit

--- server/redditController.js
+++ server/redditController.js
@@ -1,25 +1,38 @@
 var request = require('request');
+var redditArray = require('./config.js').redditArray;
 
 module.exports = {
   getFrontPage: function(req, res){
-
-    res.send();
+    var sendArray = [];
+    redditArray.forEach((el)=>{
+      request(createRequestObject(el, 'GET'), (err, response, body)=>{
+        if(err){
+          console.log('err in initial request', err);
+        }
+        var subObject = {};
+        subObject.sub = el;
+        var content = JSON.parse(body);
+        subObject.list = content.data.children.slice(0, 5);
+        sendArray.push(subObject);
+        if(sendArray.length === redditArray.length){
+          res.send(JSON.stringify(sendArray));
+        }
+        
+      });
+    });
   },
   getSub: function(req, res){
     var sub = req.params.sub;
-    var requestObject = {
-      url: `http://www.reddit.com/r/${sub}.json`,
-      method: 'GET'
-    };
-    request(requestObject, (err, response, body)=>{
+    
+    request(createRequestObject(sub, 'GET'), (err, response, body)=>{
       if(err){
         handleError(response, err.message, 'No such subreddit', 404);
       } 
       else{
-        var objOut = {sub};
+        var subObject = {sub};
         var content = JSON.parse(body);
-        objOut.list = content.data.children;
-        res.send(JSON.stringify(objOut));
+        subObject.list = content.data.children;
+        res.send(JSON.stringify(subObject));
       }
     });
 
@@ -30,4 +43,11 @@ module.exports = {
 function handleError(res, reason, message, code){
     console.log('ERROR: ' + reason);
     res.status(code || 500).json({'error': message});
+}
+
+function createRequestObject(sub, method){
+  return {
+    url: `http://www.reddit.com/r/${sub}.json`,
+    method: method
+  };
 }
