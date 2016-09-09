07e065c3257ab5a7bc38e68100774ece901609c3
komali2 <rogersjcaleb@gmail.com>
2016-09-09 15:06:42 -0700

    core: search factory sending request and getting response

--- client/js/controllers/searchController.js
+++ client/js/controllers/searchController.js
@@ -1,8 +1,23 @@
 angular.module('redditApp')
-  .factory('searchFactory', [() => {
+  .factory('searchFactory', ['$http', ($http)=>{
+    var api = {};
 
+    api.searchSub = function(value){
+      $http({
+        method: 'GET',
+        url: `/reddit/${value}`
+      })
+      .then(function(response){
+        console.log('got response', response);
+      }, function(response){});
+    };
+
+    return api;
   }])
-  .controller('searchController', ['$scope', ($scope) => {
+  .controller('searchController', 
+  ['$scope', 'searchFactory', 
+  ($scope, searchFactory) => {
 
+    $scope.searchSub = searchFactory.searchSub;
     
   }]);
